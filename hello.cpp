0e0724694197c69f3dca706d4c9bce756df8a3cc
phamgi <trungdunght2003@gmail.com>
2023-07-24 16:45:23 +0700

    thu commit

--- hello.cpp
+++ hello.cpp
@@ -1,10 +1,25 @@
-// #include <iostream>
 #include <bits/stdc++.h>
 
 using namespace std;
+
 int main() {
-    cout << "Hello!";
-    
+    vector<int> A;
+    // A[10] = 9;
+    A.push_back(3);
+    A.push_back(1);
+    A.push_back(8);
+    A.push_back(8);
+    A.push_back(8);
+    A.push_back(8);
+    A.push_back(8);
+
+    int v = A.back();
+    // printf("%d", A.);
+    // printf("%lu", A.size());
+
+    for (int i = 0; i < A.size(); i++){
+        printf("%d ", A[i]);
+    }
     return 0;
 }
     
