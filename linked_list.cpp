4ff9d1b7fec3090651d2d040c1dfb5803f082e74
khoai <trungdunght2003@gmail.com>
2023-12-13 06:29:03 +0700

    add linkedList

---
+++ linked_list.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+// #include "iostream"
+
+using namespace std;
+
+struct node {
+    int data;
+    node *next;
+};
+
+class MyLinkedList{
+public:
+    node* head;
+    MyLinkedList(){
+        head = NULL;
+    }
+
+    // int get(int index) {
+        
+    // }
+    
+    void addAtHead(int val) {
+        node moi;
+        moi.data = val;
+        moi.next = head;
+        head = &moi;
+    }
+    void print_all(){
+        node* a;
+        a = head;
+        while (a != NULL){
+            cout << a->data << endl;
+            a = a->next;
+        }
+    }
+    
+    // void addAtTail(int val) {
+        
+    // }
+    
+    // void addAtIndex(int index, int val) {
+        
+    // }
+    
+    // void deleteAtIndex(int index) {
+        
+    // }
+};
+
+
+
+
+int main(){
+
+    // node aa;
+    // aa.data = 4;
+    // node* head;
+    // head = &aa;
+    // cout << head->data;
+
+    MyLinkedList list;
+    list.addAtHead(4);
+    list.addAtHead(3);
+    list.print_all();
+    return 0;
+}
